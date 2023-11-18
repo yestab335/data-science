3e61ff30db07761c25b458a74ce3c8a11075e37a
Yasser Madouch <yestab335@gmail.com>
2023-11-17 22:59:09 -0500

    Created 1 graph of original dataset, created 1 faceted chart of password categories to strenth, filtered the strength variable from the original dataset and faceted the filtered dataset

--- passwords/main.R
+++ passwords/main.R
@@ -13,3 +13,13 @@ str(passwords)
 # Compare password category to its respected strength
 gf_boxplot(strength ~ category, data = passwords) %>%
   gf_labs(title = "Figure 1. Password categories and their strengths")
+
+gf_histogram( ~ strength, data = passwords) %>%
+  gf_facet_grid(category ~ .) %>%
+  gf_labs(title = "Figure 2. Password categories and their strengths")
+
+# Filter all values above 10 in `strength`
+strenths.filtered <- filter(passwords, strength %in% c(1:10))
+
+gf_bar(~ category, data = strenths.filtered) %>%
+  gf_facet_grid(strength ~ .)
