73fa9960f0a7e6f7c670295674210560a4519080
Yasser Madouch <yestab335@gmail.com>
2023-12-07 00:51:43 -0500

    Filtered password data frame further and omitted columns that are not within in the scope of the analysis (font size)

--- passwords/variation.R
+++ passwords/variation.R
@@ -1,4 +1,7 @@
-# Initial exploration of the variation in the dataset using a bar graph
+# Removed unspecified data from data set for the topic focus
+passwords.filtered <- strenths.filtered[-c(7,9)]
+
+# Initial exploration of the variation in the data set using a bar graph
 gf_bar( ~ category, data = strenths.filtered, fill = ~strength) %>%
   gf_labs(title = "Figure 5. Categorical count")
 
