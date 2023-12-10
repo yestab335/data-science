9bcdbcc8dbb3836dfb4a4f7f67c695e71f3f3389
Yasser Madouch <yestab335@gmail.com>
2023-12-10 02:17:25 -0500

    Added to the GLM notation, refactored code and code structure, added empty linear model and regression model based off of data frame 1, filtered all passwords ranked as the strongest from data frame 2

--- passwords/variationModeling.R
+++ passwords/variationModeling.R
@@ -1,12 +1,27 @@
 # GLM MODEL:
-#   Yi = b_0 + b_1(X_i) + e_i
+#   Y_i = b_0 + b_1(X_i) + e_i
+#   Bad_Password_i = B_0 + b_1 (Category_i) + e_i
 
-# Linear model
-linearModel <- lm(strength ~ category, data = passwords.filtered)
-linearModel
+# Empty linear model
+empty_linearModel <- lm(strength ~ NULL, data = passwords.filtered)
+empty_linearModel
+
+gf_point(strength ~ category, data = passwords.filtered) %>%
+  gf_lm()
+
+# Regression model
+passwords_model <- lm(strength ~ category, data = passwords.filtered)
+passwords_model
+
+# Predictions based off of the regression model `password_model`
+passwords.filtered$predictions <- predict(passwords_model)
+head(passwords.filtered)
+
+gf_point(strength ~ category, data = passwords.filtered) %>%
+  gf_lm()
 
 # Filters all passwords ranked with '2' and subsets into new data frame
 strongPassword <- subset(passwordStrengths, strength == 2)
 
-
-strongPasswordLinearModel <- lm(strength, data = strongPassword)
+strongPasswordLinearModel <- lm(strength ~ NULL, data = strongPassword)
+strongPasswordLinearModel
