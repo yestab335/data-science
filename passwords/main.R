524ba492d4cbe2194598894d7080e3277b855c11
Yasser Madouch <yestab335@gmail.com>
2023-12-10 20:54:27 -0500

    Refactored labs() to title case

--- passwords/main.R
+++ passwords/main.R
@@ -10,18 +10,18 @@ str(passwords)
 
 # Compare password category to its respected strength
 gf_boxplot(strength ~ category, data = passwords) %>%
-  gf_labs(title = "Figure 1. Password categories and their strengths")
+  gf_labs(title = "Figure 1. Password Categories and their Strengths")
 
 gf_histogram( ~ strength, data = passwords) %>%
   gf_facet_grid(category ~ .) %>%
-  gf_labs(title = "Figure 2. Password categories and their strengths")
+  gf_labs(title = "Figure 2. Password Categories and their Strengths")
 
 # Filter all values above 10 in `strength`
 strenths.filtered <- filter(passwords, strength %in% c(1:10))
 
 gf_bar(~ category, data = strenths.filtered) %>%
   gf_facet_grid(strength ~ .) %>%
-  gf_labs(title = "Figure 3. Password categories and their strengths filtered")
+  gf_labs(title = "Figure 3. Password Categories and their Strengths Filtered")
 
 gf_jitter(strength ~ category, data = strenths.filtered) %>%
-  gf_labs(title = "Figure 4. Password categories and their strengths filtered")
+  gf_labs(title = "Figure 4. Password Categories and their Strengths Filtered")
