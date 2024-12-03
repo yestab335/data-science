158dc7477cd292d30ec768df5a822d30195b47a1
Adam <yestab335@gmail.com>
2024-12-03 11:55:08 -0500

    Added functions to easily create density plots and histograms. As well as defined a default size for all plots

--- ChangeOfCareer/visualizations.py
+++ ChangeOfCareer/visualizations.py
@@ -1,13 +1,18 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def scatter(data, x, y, hueColor=None):
-  x = x
-  y = y
+plt.subplots(1, 1, figsize=(12, 6))
 
+def scatter(data, x, y, hueColor=None):
   sns.scatterplot(data=data, x=x, y=y, hue=hueColor)
-  plt.title(f'{x} vs. {y}')
-  plt.xlabel(x)
-  plt.ylabel(y)
+  plt.tight_layout()
+  plt.show()
+
+def density(data):
+  sns.kdeplot(data)
+  plt.show()
+
+def histogram(distVariable, density=None):
+  plt.hist(distVariable, density=density)
   plt.tight_layout()
   plt.show()
