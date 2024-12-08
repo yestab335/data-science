28480dd643a89c09604c648fd3bc54f8981ca597
Adam <yestab335@gmail.com>
2024-12-08 01:44:49 -0500

    Cleaned up code for easy readability

--- ChangeOfCareer/main.py
+++ ChangeOfCareer/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from datacleaner import *
 from visualizations import *
-import matplotlib.pyplot as plt
 
 def main():
   data = pd.read_csv('career_change_prediction_dataset.csv')
@@ -10,16 +9,9 @@ def main():
   print(data.info())
 
   # View the distribution of the target variable
-  # plt.subplots(1, 1, figsize=(12, 6))
-
-  histogram(data['Current Occupation'], True)
-  plt.title('Figure 1. Data Distribution of Target Variable')
-
-  # TODO: Carrer Change Interest - reassign 1 to yes and 0 to no
-  # data['Career Change Interest'] = data['Career Change Interest'].map({
-  #   0: 'No',
-  #   1: 'Yes',
-  # })
+  histogram()
+  scatter()
+  density()
 
 if __name__ == '__main__':
   main()
